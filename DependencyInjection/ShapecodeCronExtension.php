6dd36220c17404ad7eac677e0b7277699cbc34bc
Nikita Loges <n.loges@tenolo.de>
2015-06-23 14:50:44 +0200

    remove configuration class

--- DependencyInjection/ShapecodeCronExtension.php
+++ DependencyInjection/ShapecodeCronExtension.php
@@ -20,9 +20,6 @@ class ShapecodeCronExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
