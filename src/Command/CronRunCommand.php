85c1a24d748c9ff54bf436c62fe58a44e2a5c84d
Steven Brookes <stevenjbrookes@yahoo.co.uk>
2016-09-01 10:38:10 +0100

    Move next run calculation before running jobs

--- src/Command/CronRunCommand.php
+++ src/Command/CronRunCommand.php
@@ -61,8 +61,17 @@ class CronRunCommand extends BaseCommand
         }
 
         $jobCount = count($jobsToRun);
-        $output->writeln('Running '.$jobCount.' jobs:');
+        $output->writeln('Running ' . $jobCount . ' jobs:');
 
+        // Update the job with it's next scheduled time
+        $now = new \DateTime();
+        foreach ($jobsToRun as $job) {
+            $job->calculateNextRun();
+            $job->setLastUse($now);
+        }
+        $this->getEntityManager()->flush();
+
+        // Run the jobs
         foreach ($jobsToRun as $job) {
             $this->runJob($job, $output);
         }
@@ -98,11 +107,6 @@ class CronRunCommand extends BaseCommand
             return;
         }
 
-        // And update the job with it's next scheduled time
-        $job->calculateNextRun();
-        $job->setLastUse(new \DateTime());
-        $this->getEntityManager()->flush();
-        
         $emptyInput = new ArrayInput(array(
             'command' => $job->getCommand()
         ));
@@ -168,4 +172,5 @@ class CronRunCommand extends BaseCommand
     {
         return $this->get('debug.stopwatch');
     }
+
 }
