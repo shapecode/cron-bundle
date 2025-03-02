28940f80889d6ae4163c6f6fd89167c61a3883a6
Nikita Loges <git@loges.one>
2025-03-02 19:49:42 +0100

    bump php to 8.3

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,10 +3,7 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
-use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-$config = new Configuration();
-
-return $config
+return (new Configuration())
     // Adjusting scanned paths
     ->addPathToScan(__DIR__ . '/tests', true);
